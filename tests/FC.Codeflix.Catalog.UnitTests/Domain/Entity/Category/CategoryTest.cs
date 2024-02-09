c9b6409d2afa83c5cad023938f96e15ffd1f1477
Thiago Ripardo de Lima <thiago.ripardo.86@gmail.com>
2024-02-09 15:16:09 -0400

    upload project

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+using DomainEntity = FC.Codeflix.Catalog.Entity;
+namespace FC.Codeflix.Catalog.UnitTests.Domain.Entity.Category;
+
+public class CategoryTest
+{
+    [Fact(DisplayName = nameof(Instantiate))]
+    [Trait("Domain", "Category - Aggregates")]
+    public void Instantiate()
+    {
+        // Arrange
+        var validData = new
+        {
+            Name = "Category name",
+            Description = "Description Category"
+        };
+
+        //Act
+        var category = new DomainEntity.Category(validData.Name, validData.Description);
+
+        //Assert
+        Assert.NotNull(category);
+        Assert.Equal(validData.Name, category.Name);
+        Assert.Equal(validData.Description, category.Description);
+    }
+}
+
